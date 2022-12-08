34b5e1101c95f011fbfe0652cce655cc44aaebd5
Levrk <levrolandkalb@gmail.com>
2022-12-08 13:43:10 -0500

    Added Documentation and more info for ReadMe.txt

--- Player.java
+++ Player.java
@@ -1,8 +1,5 @@
 public class Player{
 
-    private int ppg;
-    private int rpg;
-    private int apg;
     private int fg;
     private int threeP;
     public String name;
@@ -12,13 +9,12 @@ public class Player{
     public int assists;
     public Team team;
     
-    public Player (int ppg, int rpg, int apg, int fg, int threeP, String name) {
-        this.ppg = ppg;
-        this.rpg = rpg;
-        this.apg = apg;
+    public Player (int fg, int threeP, String name) {
+        
         this.fg = fg;
         this.threeP = threeP;
         this.name = name;
+        //these stats initialize to 0;
         this.points = 0;
         this.rebounds = 0;
         this.tov = 0;
@@ -27,6 +23,8 @@ public class Player{
     }
 
     public Player shoot(Team team){
+        //determines if the shot is a 2 or 3 based on player 3p %
+        //fix it so kevon looney never shoots 3s (needs work)
         if (this.threeP > 40) {
             int chance = (int)(Math.random() * ((9) + 1));
             if (chance > 3) return this.shoot3(team);
@@ -46,47 +44,57 @@ public class Player{
             
     }
     
+    
     public Player shoot2 (Team team) { 
+        //team argument is used so the assister will not be the shooter
+        //figure out if the shot is successful
         int chance = (int)(Math.random() * ((100- 0) + 1));
         boolean score = chance < this.fg;
         if (score == true) {
+        //check if basket was made, update points, print
         points = points + 2 ;
         System.out.println(printMade2());
         if (chance < team.assistP){
+            //check if assist,get passer, print, update stats
             Player passer = team.getPasser(this);
             System.out.println(team.printAssist(passer));
             System.out.println("");
             passer.assists++;
         }
+        //return shooter
         return this;
         }
         else System.out.println(printMissed2());
-        chance = (int)(Math.random() * (100 + 1));
         return null;
         
         
     }
 
     public Player shoot3 (Team team) { 
+        //team argument is used so the assister will not be the shooter
+        //figure out if the shot is successful
         int chance = (int)(Math.random() * ((100- 0) + 1));
         boolean score = chance < this.threeP;
         if (score == true) {
+        //check if basket was made, update points, print
         points = points + 3 ;
         System.out.println(printMade3());
         if (chance < team.assistP){
+            //check if assist,get passer, print, update stats
             Player passer = team.getPasser(this);
             System.out.println(team.printAssist(passer));
             System.out.println("");
             passer.assists++;
         }
+        //return shooter
         return this;
         }
         else System.out.println(printMissed2());
-        chance = (int)(Math.random() * (100 + 1));
         return null;
     }
 
     public String printMade3 (){
+        //returns string for made 3
         int chance = (int)(Math.random() * ((2) + 1));
         if (chance == 0) return name + " cashes in from deep!";
         else if (chance == 1) return name + " hits it from long range!";
@@ -94,6 +102,7 @@ public class Player{
     }
 
     public String printMissed3 (){
+        //returns string for missed 3
         int chance = (int)(Math.random() * ((2) + 1));
         if (chance == 0) return name + " is off from deep";
         else if (chance == 1) return name + " misses from long range";
@@ -101,6 +110,7 @@ public class Player{
     }
 
     public String printMade2 (){
+        //returns string for made 2
         int chance = (int)(Math.random() * ((2) + 1));
         if (chance == 0) return name + " converts from close range!";
         else if (chance == 1) return name + " with the finish";
@@ -108,6 +118,7 @@ public class Player{
     }
 
     public String printMissed2 (){
+        //returns string for missed 2
         int chance = (int)(Math.random() * ((2) + 1));
         if (chance == 0) return name + " is off from close range";
         else if (chance == 1) return name + " misses the shot";
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,13 +1,40 @@
-NBA simulator in it's early stages 
+NBA simulator - work in progress
+
+description:
+Program currently capable of simulating a game between the Celtics and Warriors (missing the outcome)
+
+(player stats from ESPN 2022)
+(feel free to use/modify/contribute to this code as you see fit!)
+
+Major Limitations:
+- no substitutions (only 5 man rotation)(no plans to address this)
+- inputing team/player data is tedius (definetely want to address this)
+- game exists outside of the scope of time (not sure if I want to/can fix this)
+
+Minor Limitations:
+- no injury mechanic (no plans to address this)
+- turnovers are impossible after offensive rebounds (not sure if I want to fix this)
 
 
 
 issues to fix:
-[ ]  fix the run play function
+[!]  fix the run play function
     - rework the shoot function to return an array of passer and shooter?
-[ ]  fix rebounding stats
-[ ]  fix looping shots after miss
+[!]  fix rebounding stats
+[!]  fix looping shots after miss
 [ ]  keep track of team points
+[ ]  print final scores and winner
+[ ]  fix the way player/team data is inputted
+[!]  add documentation
 
 
-test to make sure I am using GitHub properly
+Things to add
+[ ]  make a .txt file for inputting team info
+[ ]  more options for "dialogue"
+
+! == completed
+
+CREDITS:
+LEV ROLAND-KALB
+CLARK UNIVERSITY
+
