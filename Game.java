dcd7f230f7f3cfb8879f9c89ee6f896c4819c8fb
Levrk <levrolandkalb@gmail.com>
2022-12-08 11:33:55 -0500

    commiting changes from VS code

---
+++ Game.java
@@ -0,0 +1,93 @@
+public class Game {
+    
+
+    public Team teamA;
+    public Team teamb;
+
+
+
+    public Game (Team teamA, Team teamB){
+        this.teamA = teamA;
+        this.teamA = teamA;
+    }
+
+
+
+    public static void playQuarter(Team teamA,Team teamB,Integer q){
+
+        System.out.println("Quarter "+q);
+
+        for (int i = 0;i<1;i++){
+            teamA.runPlay(teamB);
+            teamB.runPlay(teamA);
+        }
+        
+        System.out.println();
+        System.out.println(teamA);
+        System.out.println(teamB);
+        
+
+
+    }
+
+    public static Team endGame (Team teamA,Team teamB){
+        //System.out.println(teamA.name + ": " + teamA.points);
+        //System.out.println(teamB.name + ": " + teamB.points);
+        
+        //temp
+        return teamA;
+    }
+
+
+
+    public static Team playGame(Team teamA,Team teamB){
+
+
+        playQuarter(teamA, teamB,1);
+        playQuarter(teamA, teamB,2);
+        playQuarter(teamA, teamB,3);
+        playQuarter(teamA, teamB,4);
+
+        endGame(teamA,teamB);
+        
+        //temp
+        return teamA;
+    
+    }
+
+   
+    public static void main (String[] args){
+        Player JasonTatum = new Player(31,8,4,48,36,"Jason Tatum");
+        Player JaylenBrown = new Player(27,7,4,50,35,"Jaylen Brown");
+        Player MarcusSmart = new Player(12,3,8,45,36, "Marcus Smart");
+        Player DerrickWhite = new Player(11,3,3,47,43,"Derrick White");
+        Player AlHorford = new Player(10,6,3,54,47, "Al Horford");
+        int[] celticsShooters = {0,0,0,0,1,1,1,2,2,3,4};
+        int[] celticsRebounders = {0,0,0,1,1,2,2,3,4,4,4};
+        int[] celticsPassers = {0,0,1,1,2,2,2,3,4};
+        Player[] celticsRoster = {JasonTatum,JaylenBrown,MarcusSmart,DerrickWhite,AlHorford};
+        Team celtics = new Team("Celtics",celticsRoster,celticsShooters,celticsRebounders,celticsPassers,23,73,63,14);
+        
+        Player StephenCurry = new Player(30,7,7,50,43,"Stephen Curry");
+        Player KlayThompson = new Player(18,4,3,40,40,"Klay Thompson");
+        Player DraymondGreen = new Player(9,6,7,56,31, "Draymond Green");
+        Player AndrewWiggins = new Player(19,5,2,51,45,"Andrew Wiggins");
+        Player KevonLooney = new Player(6,7,3,67,10, "Kevon Looney");
+        int[] warriorsShooters = {0,0,0,1,1,1,2,3,3,4};
+        int[] warriorsRebounders = {0,1,2,2,3,3,4,4};
+        int[] warriorsPassers = {0,0,1,2,2,3,4};
+        Player[] warriorsRoster = {StephenCurry,KlayThompson,DraymondGreen,AndrewWiggins,KevonLooney};
+        Team warriors = new Team("Warriors",warriorsRoster,warriorsShooters,warriorsRebounders,warriorsPassers,26,71,70,16);
+        
+        playGame(celtics,warriors);
+        System.out.println();
+        
+        
+
+
+
+
+
+
+    }
+}
---
+++ Team.java
@@ -0,0 +1,118 @@
+public class Team {
+    
+    private String name;
+    private Player[] team;
+    private int[] shooters;
+    private int[] passers;
+    private int[] rebounders;
+    private int Dreb;
+    private int Oreb;
+    private int tov;
+    private int assistP;
+    public int points;
+
+    public Team (String name, Player[] team, int[] shooters, int[] passers, int[] rebounders, int Oreb, int Dreb, int assistP, int tov){
+        this.name = name;
+        this.team = team;
+        this.shooters = shooters;
+        this.passers = passers;
+        this.rebounders = rebounders;
+        this.Dreb = Dreb;
+        this.Oreb = Oreb;
+        this.tov = tov;
+        this.assistP = assistP;
+        this.points = points;
+    }
+
+
+    @Override
+    public String toString (){
+        String output = "";
+        for (Player p : team)
+            output = output + (p.name + " " +Integer.toString(p.points) + " Points, "  + Integer.toString(p.rebounds) + " Rebounds, " + Integer.toString(p.assists)+ " Assists, " + Integer.toString(p.tov) + " Turnovers, "  + "\n");
+        return output;
+    }
+
+    public void printTeam(){
+        
+
+
+
+    }
+
+    public Player getShooter(){
+        int chance = (int)(Math.random() * ((shooters.length-1) + 1));
+        return team[shooters[chance]];
+    }
+
+    public Player getRebounder(){
+        int chance = (int)(Math.random() * ((rebounders.length-1) + 1));
+        return team[rebounders[chance]];
+    }
+
+    public Player getPasser(Player shooter){
+        //returns the passer
+        int chance = (int)(Math.random() * ((passers.length-1) + 1));
+        while (team[passers[chance]]==shooter){
+            chance = (int)(Math.random() * ((passers.length-1) + 1));
+        };
+        return team[passers[chance]];
+    }
+
+    public Player getRandomPlayer(){
+        //returns random player
+        int chance = (int)(Math.random() * ((team.length-1) + 1));
+        return team[chance];
+    }
+
+    public String printAssist(Player passer){
+        int chance = (int)(Math.random() * ((2) + 1));
+        if (chance == 0) return "Off the pass from " + passer.name;
+        else if (chance == 1) return "Off the feed from " + passer.name;
+        else return "Assisted by " + passer.name;
+    }
+
+    public String printStartPlay (){
+        int chance = (int)(Math.random() * ((2) + 1));
+        if (chance == 0) return name + " take the ball up the court";
+        else if (chance == 1) return name + " looking to score";
+        else return name + " ball";
+    }
+
+
+
+    public void runPlay (Team opponent) {
+        System.out.println(printStartPlay());
+        int chance = (int)(Math.random() * ((100) + 1));
+        if (chance < tov) {
+            System.out.println(name + " turn over the ball");
+            System.out.println();
+            getRandomPlayer().tov++;
+            return;
+        }
+        //so we can remember the value for assist
+        Player shooter = getShooter();
+        while (shooter.shoot()==false){
+            chance = (int)(Math.random() * ((Oreb + opponent.Dreb) + 1));
+            if (chance > Oreb) {
+                System.out.println(opponent.name + " come up with the rebound");
+                System.out.println();
+                return;
+            }
+            else 
+                System.out.println(name + " grab the rebound");
+            if (shooter.shoot()==false) shooter = getShooter();
+            
+    }
+        chance = (int)(Math.random() * (100 + 1));
+        if (chance < assistP){
+            Player passer = getPasser(shooter);
+            System.out.println(printAssist(passer));
+            passer.assists++;
+        }
+        System.out.println();
+    }
+
+    public static void main (String[] args){
+    }
+}
