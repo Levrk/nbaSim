993374650ed358001581cb9f9a9a388333b9df73
Levrk <levrolandkalb@gmail.com>
2022-12-08 18:34:33 -0500

    Program now takes team names as arguments Created setMatchup() to clean up the Main function

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     
 
 public static Team celticsSetup(){
+    //sets up celtics instances
     Player JasonTatum = new Player(48,36,"Jason Tatum");
     Player JaylenBrown = new Player(50,35,"Jaylen Brown");
     Player MarcusSmart = new Player(45,36, "Marcus Smart");
@@ -15,7 +16,7 @@ public static Team celticsSetup(){
     return celtics;
 }
 public static Team warriorsSetup(){
-    
+    //sets up warriors instances
     Player StephenCurry = new Player(50,43,"Stephen Curry");
     Player KlayThompson = new Player(40,40,"Klay Thompson");
     Player DraymondGreen = new Player(56,31, "Draymond Green");
@@ -29,14 +30,51 @@ public static Team warriorsSetup(){
     return warriors;
 }
 
+public static Team cavsSetup(){
+    //sets up cavs instances
+    Player DMitchell = new Player(50,42,"Donovan Mitchell");
+    Player DGarland = new Player(44,40,"Darius Garland");
+    Player EMobley = new Player(56,22, "Evan Mobley");
+    Player JAllen = new Player(60,0,"Jarret Allen");
+    Player KLove = new Player(42,39, "Kevin Love");
+    int[] cavsShooters = {0,0,0,1,1,2,3,4};
+    int[] cavsRebounders = {0,1,2,2,3,3,4,4};
+    int[] cavsPassers = {0,0,1,1,1,2,3,4};
+    Player[] cavsRoster = {DMitchell,DGarland,EMobley,JAllen,KLove};
+    Team cavs = new Team("Cavaliers",cavsRoster,cavsShooters,cavsRebounders,cavsPassers,28,74,58,15);
+    return cavs;
+}
+
+
+
+public static Team[] setMatchup(String arg0, String arg1){
+    //sets up matchup as array of two teams
+
+    //setting up team instances
+    Team warriors = warriorsSetup();
+    Team celtics = celticsSetup();
+    Team cavs = cavsSetup();
+    Team teamA = null;
+    Team teamB = null;
+    //checking CL arguments
+    if (arg0.equals("warriors"))  teamA = warriors;
+    if (arg0.equals("celtics")) teamA = celtics;
+    if (arg0.equals("cavs")) teamA = cavs;
+    if (arg1.equals("warriors")) teamB = warriors;
+    if (arg1.equals("celtics")) teamB = celtics;
+    if (arg1.equals("cavs")) teamB = cavs;
+    Team[] teams = {teamA,teamB};
+    return teams;
+
+}
+
+
 
 
     public static void main(String[] args){
        
-        Team warriors = warriorsSetup();
-        Team celtics = celticsSetup();
-
-        Game game = new Game(celtics,warriors);
-        game.playGame(celtics,warriors);
+        Team[] teams = setMatchup(args[0],args[1]);
+        Game game = new Game(teams[0],teams[1]);
+        game.playGame(teams[0],teams[1]);
     }
 }
